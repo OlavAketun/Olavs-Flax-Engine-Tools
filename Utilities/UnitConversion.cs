c8965cbabcd96bd7074aac6ff87e1cad659ebb22
olavaketun <olav.aketun@outlook.com>
2026-02-12 13:26:42 +0100

    ADD UnitConversion
    A unit conversion utility for Flax, useful for converting from Flax native units to meter and so on

---
+++ Utilities/UnitConversion.cs
@@ -0,0 +1,7 @@
+﻿namespace Utilities;
+public static class UnitConstants
+{
+    public const float Cm2ToM2 = 0.0001f;
+    public const float CmToM = 0.01f;
+    public const float MToCm = 100f;
+}
