179b45c113e07122b11d8937d180a95959833658
olavaketun <olav.aketun@outlook.com>
2026-02-14 13:16:01 +0100

    UPDATE mathtools
    added minor comment

--- Utilities/MathTools.cs
+++ Utilities/MathTools.cs
@@ -9,7 +9,7 @@ public class MathTools
         return float.Sqrt((vec3.X * vec3.X) + (vec3.Y * vec3.Y) + (vec3.Z * vec3.Z));
     }
 
-    public static Vector3 ClampMagnitude(Vector3 vec3, float maxLength)
+    public static Vector3 ClampMagnitude(Vector3 vec3, float maxLength)//Unity-like clamp magnitude
     {
         Vector3 result;
         if (Magnitude(vec3) > maxLength * maxLength)
